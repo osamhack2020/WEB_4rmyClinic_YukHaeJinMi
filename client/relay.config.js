3bf35e3c690d48f1231c83c25d23efc7471f2f0e
이성빈 <lsb918@uos.ac.kr>
2020-09-29 01:01:04 +0900

    connected

---
+++ client/relay.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  src: "./src",
+  schema: "./schema.graphql",
+  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+  language: "typescript"
+}
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,12 +7,31 @@ import {
 } from 'react-router-dom';
 
 import { Main, NotFound, Post, Profile, SignIn, SignUp } from './Routes';
-
+import { QueryRenderer, graphql } from "react-relay";
+import { AppQuery } from "./__generated__/AppQuery.graphql";
+import environment from "./_lib/environment";
 function App() {
   return (
     <Router>
       <header>header</header>
-
+      <QueryRenderer<AppQuery>
+        environment={environment}
+        variables={{}}
+        query={graphql`
+          query AppQuery {
+            recentPosts(first: 10) {
+              edges {
+                node {
+                  isPrivate
+                }
+              }
+            }
+          }
+        `}
+        render={({ props, error, retry }) => {
+          return <div>{error && <div><p>{error.message}</p><p>{"아직 데이터가 없어요."}</p></div>}</div>
+        }}
+      />
       <Switch>
         <Route exact path="/" render={(props) => <Main {...props} />} />
         <Route path="/post/:id" render={(props) => <Post {...props} />} />
