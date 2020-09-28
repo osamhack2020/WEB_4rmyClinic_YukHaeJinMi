a193464586a303578c2f737938403a20deb1875e
유준 홍 <hyjhyj0901@naver.com>
2020-09-28 14:42:53 +0000

    about 페이지추가/ 간단한 header 디자인

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
 import './App.scss';
+import logo from './logo2.png';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 
-import { Main, NotFound, Post, Profile, SignIn, SignUp } from './Routes';
+import { Main, NotFound, Post, Profile, SignIn, SignUp , About } from './Routes';
 
 function App() {
   return (
     <Router>
-      <header>header</header>
+      <header>
+        <div className="menu-wrapper">
+          <div className="header-logo">
+            <Link to="/">
+              <img src={logo} alt = "logo" className = "logo"/>
+            </Link>
+          </div>
+          <div className="menu-holder">
+            <nav className="menu-container">
+              <ul className="menu-ul">
+                <Link to="/">Home</Link>
+                <Link to="/About">About</Link>
+                <Link to="/">Counseler</Link>
+                <Link to="/post/:id">Community</Link>
+                <Link to="/profile/:id">profile</Link>
+              </ul>
+            </nav>
+          </div>
+          <div className="clear"></div>
+        </div>
+      </header>
 
       <Switch>
         <Route exact path="/" render={(props) => <Main {...props} />} />
         <Route path="/post/:id" render={(props) => <Post {...props} />} />
+        <Route path="/About" render={(props) => <About {...props} />} />
         <Route path="/signin" render={(props) => <SignIn {...props} />} />
         <Route path="/signup" render={(props) => <SignUp {...props} />} />
         <Route path="/profile/:id" render={(props) => <Profile {...props} />} />
