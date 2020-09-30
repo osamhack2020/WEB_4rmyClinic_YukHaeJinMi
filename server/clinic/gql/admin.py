a03dc61e55d146ef73f3272105b5a7fb7c931e58
이성빈 <lsb918@uos.ac.kr>
2020-09-30 18:22:51 +0900

    model type

--- server/clinic/gql/admin.py
+++ server/clinic/gql/admin.py
@@ -21,7 +21,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'division','rank' 'password1', 'password2')}
+            'fields': ('email', 'division','rank', 'password1', 'password2')}
          ),
     )
     search_fields = ('email','division','rank')
