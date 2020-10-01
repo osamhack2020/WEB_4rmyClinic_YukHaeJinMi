c8d9e3343c1b90497a87e55eb5ceade255bc7f2e
유준 홍 <hyjhyj0901@naver.com>
2020-10-01 15:02:55 +0000

    Card component add/ Main page design

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-import logo from '../assets/logo2.png';
+import logo from '../assets/logo2_bgremoved.png';
 
 type HeaderProps = {
   userSignedId?: boolean,
