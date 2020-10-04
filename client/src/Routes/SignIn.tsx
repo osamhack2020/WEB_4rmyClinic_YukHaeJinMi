7ce09c2c1bb320ba09fa154545bfaf21b0750d48
이성빈 <lsb918@uos.ac.kr>
2020-10-04 12:50:27 +0900

    add responsive properties

--- client/src/Routes/SignIn.tsx
+++ client/src/Routes/SignIn.tsx
@@ -1,36 +1,34 @@
 import React from 'react';
 import { RouteComponentProps } from "react-router";
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo2_bgremoved.png';
-import '../scss/SignIn.scss';
+// import logo from '../assets/logo2_bgremoved.png';
+import '../scss/Sign.scss';
 
 export function SignIn(props: RouteComponentProps) {
   return (
-    <div>
-      <div className="signin">
-        <div className="logo">
-          <img src={logo} alt="logo"/>
-        </div>
-        <div className="blank"></div>
-        <div className="form-container">
-          <form name="signin" method="post">
-            <h1>계정을 입력하세요</h1>
-            <p>
-              <h3>이메일</h3>
-              <input type="email" name="email" />
-            </p>
-            <p>
-              <h3>비밀번호</h3>
-              <input type="password" name="password" />
-            </p>
+    <div className="root">
+      <div className="sign">
+        {/* <img src={logo} className="logo" alt="logo" /> */}
 
-            <hr /><hr />
-            <input type='submit' value='로그인' />
-          </form>
-          <div className="blank"></div>
-          <p>아직 계정이 없으신가요? <Link to="/signup">회원가입 하기</Link></p>
-        </div>  
+        <form className="sign-form" name="signin" method="post">
+          <h1>계정을 입력하세요</h1>
+
+          <div className="box">
+            <label htmlFor="email">이메일</label>
+            <input className="box-input" type="email" name="email" />
+          </div>
+
+          <div className="box">
+            <label htmlFor="password">비밀번호</label>
+            <input className="box-input" type="password" name="password" />
+          </div>
+
+          <input className="box-btn" type='submit' value='로그인' />
+        </form>
       </div>
+
+      <p>아직 계정이 없으신가요? <Link to="/signup">회원가입 하기</Link></p>
+
     </div>
   )
 } 
