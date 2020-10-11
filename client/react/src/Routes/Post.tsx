07dcd24dea29735e2422468d5fb89dfbd4290b3d
Kick-snare <hyjhyj0901@naver.com>
2020-10-11 08:26:16 +0000

    design post page

--- client/react/src/Routes/Post.tsx
+++ client/react/src/Routes/Post.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { RouteComponentProps } from "react-router";
+import { Link } from 'react-router-dom';
 import { QueryRenderer, graphql } from "react-relay";
 import environment from "../_lib/environment";
 import { MainQuery } from "./__generated__/MainQuery.graphql";
 import CardContainer from "../Components/CardContainer";
-import "../scss/Main.scss";
+import "../scss/Post.scss";
 
 type postParams = {
   id: string,
@@ -12,19 +13,53 @@ type postParams = {
 
 export function Post(props: RouteComponentProps) {
   return (
-    <div className="Post">
-      <QueryRenderer<MainQuery>
-        environment={environment}
-        variables={{}}
-        query={graphql`
-          query PostQuery {
-            ...CardContainer_cards
-          }
-        `}
-        render={({ props, error, retry }) => {
-          return props && <CardContainer cards={props} />
-        }}
-      />
+    <div className="root">
+      <h1>커뮤니티</h1>
+      <div className="tag">
+        <h2>태그</h2>
+        <div className="tag-container">
+          <a href="#" className="tag-card">#선임</a>
+          <a href="#" className="tag-card">#후임</a>
+          <a href="#" className="tag-card">#애인</a>
+          <a href="#" className="tag-card">#도박</a>
+          <a href="#" className="tag-card">#간부</a>
+          <a href="#" className="tag-card">#선임</a>
+          <a href="#" className="tag-card">#후임</a>
+          <a href="#" className="tag-card">#애인</a>
+          <a href="#" className="tag-card">#도박</a>
+          <a href="#" className="tag-card">#간부</a>
+          <a href="#" className="tag-card">#선임</a>
+          <a href="#" className="tag-card">#후임</a>
+          <a href="#" className="tag-card">#애인</a>
+          <a href="#" className="tag-card">#도박</a>
+          <a href="#" className="tag-card">#간부</a>
+          <a href="#" className="tag-card">#선임</a>
+          <a href="#" className="tag-card">#후임</a>
+          <a href="#" className="tag-card">#애인</a>
+          <a href="#" className="tag-card">#도박</a>
+          <a href="#" className="tag-card">#간부</a>
+          <a href="#" className="tag-card">#선임</a>
+        </div>
+      </div>
+      <br/><hr />
+      <div className="Post">
+        <div className="Post-box">
+          <h1>최근 고민</h1>
+          <Link to="/Post:id">고민작성하기</Link>
+        </div>
+        <QueryRenderer<MainQuery>
+          environment={environment}
+          variables={{}}
+          query={graphql`
+            query PostQuery {
+              ...CardContainer_cards
+            }
+          `}
+          render={({ props, error, retry }) => {
+            return props && <CardContainer cards={props} />
+          }}
+        />
+      </div>
     </div>
   )
 }
