f09b2ad65a0ccf70bf2a55490a2798523a735522
이성빈 <lsb918@uos.ac.kr>
2020-10-11 14:31:09 +0900

    Revert "react app server"
    This reverts commit 6d1f51bf86ace24c0200cffc1aebdc1e70eb5d81.

--- staticServer/deploy.sh
+++
@@ -1,4 +0,0 @@
-# cd && ./manage.py dumpdata > fixture.json && cd ../
-docker-compose build && \
-docker-compose push && \
-gcloud --project=pitapat run deploy client --image gcr.io/pitapat/client --platform managed --region asia-northeast1
--- staticServer/server/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/osamhack2020/web_4rmyclinic_yukhaejinmi/server
-
-go 1.13
