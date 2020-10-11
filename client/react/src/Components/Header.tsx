53a6add42c6d2eefa849bc938b2fb8d91bb6717b
Kick-snare <hyjhyj0901@naver.com>
2020-10-11 13:54:58 +0000

    header mobile

--- client/react/src/Components/Header.tsx
+++ client/react/src/Components/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import '../scss/header.scss';
-import logo from '../assets/logo2_bgremoved.png';
+import logo from '../assets/logo_col.svg';
+import logo_row from '../assets/logo_row.svg';
 import { AuthContext } from "./AuthContextProvider";
 import { postCreate } from "../_lib/mutations";
 
@@ -17,9 +18,14 @@ export default function Header(props: HeaderProps) {
       {({ verified, email, logout }) =>
         <header>
           <div className="menu-wrapper">
-            <div className="header-logo">
+            <div className="header-logo-d">
               <Link to="/">
                 <img src={logo} alt="logo" className="logo" />
+              </Link>
+            </div>
+            <div className="header-logo-m">
+              <Link to="/">
+                <img src={logo_row} alt="logo" className="logo" />
               </Link>
             </div>
             <div className="menu-holder">
@@ -36,7 +42,7 @@ export default function Header(props: HeaderProps) {
             <div className="header-btn">
               {verified && email ? <div>{email} <p onClick={logout}>로그아웃하기</p></div> : <Link to="/signin">로그인 하기</Link>}
             </div>
-
+          
             {verified && email &&
               <div className="test" onClick={() => postCreate({ title: "test", content: "test" })}>
                 TEST : 클릭하여 포스트 생성
