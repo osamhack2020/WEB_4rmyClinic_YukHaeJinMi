bf37ab40b3f8495a59890fd55f82eabcdf201fc9
root <rubinstory@naver.com>
2020-10-13 11:23:15 +0900

    post fixed

--- client/react/src/Routes/Post.tsx
+++ client/react/src/Routes/Post.tsx
@@ -17,10 +17,6 @@ export function Post(props: RouteComponentProps) {
   return (
     <AuthContext.Consumer>
       {({ verified, }) =>
-        <div className="Post-root">
-          <h1>커뮤니티</h1>
-          <div className="tag">
-            <h2>태그</h2>
             <QueryRenderer<PostQuery>
               environment={environment}
               variables={{}}
@@ -40,25 +36,29 @@ export function Post(props: RouteComponentProps) {
                 render={({props, error, retry}) => {
                   const tags = props?.allTags?.edges;
                   return(
-                    <div className="tag-container">
-                    {tags?.map((e) =>
-                      <a href="##" className="tag-card">#{e?.tag?.name}</a>
-                    )}
-                    </div>
-                  );
+                  	<div className="Post-root">
+          				<h1>커뮤니티</h1>
+          					<div className="tag">
+            					<h2>태그</h2>
+                    			<div className="tag-container">
+                    				{tags?.map((e) =>
+                      				<a href="##" className="tag-card">#{e?.tag?.name}</a>
+                    				)}
+                    			</div>
+                    		</div>
+          					<br /><br />
+          					<div className="Post">
+            					<div className="Post-box">
+              						<h1>최근 고민</h1>
+              						<Link to={verified ? "/newpost" : "/signin"}>고민작성하기</Link>
+            					</div>
+            					{props && <CardContainer cards={props} />}
+          					</div>
+        				</div>
+                  	);
                 }
-              }
-            />
-          </div>
-          <br /><br />
-          <div className="Post">
-            <div className="Post-box">
-              <h1>최근 고민</h1>
-              <Link to={verified ? "/newpost" : "/signin"}>고민작성하기</Link>
-            </div>
-            {/*props && <CardContainer cards={props} />*/}
-          </div>
-        </div>
+            }	
+        />
       }
     </AuthContext.Consumer>
   );
