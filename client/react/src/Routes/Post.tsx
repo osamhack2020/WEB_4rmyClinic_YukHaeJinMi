3545f3f7f3a1e0850ab05c475dbec0c18fa3d73c
Kick-snare <hyjhyj0901@naver.com>
2020-10-17 13:40:56 +0000

    several Post layout design

--- client/react/src/Routes/Post.tsx
+++ client/react/src/Routes/Post.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { RouteComponentProps } from "react-router";
 import { QueryRenderer, graphql } from "react-relay";
 import environment from "../_lib/environment";
+import { Link } from 'react-router-dom';
 import { AuthContext } from "../Components/AuthContextProvider";
 import { PostQuery } from "./__generated__/PostQuery.graphql";
 import "../scss/Post.scss";
@@ -42,10 +43,30 @@ export function Post(props: RouteComponentProps<postParams>) {
 
             return (
               <div className="Post-root">
-                <p>제목 : {props?.post?.title}</p>
-                <p>내용 : {props?.post?.content}</p>
-                <p>작성자 : {props?.post?.author.email}</p>
-                {/* TODO : 태그 / 좋아요 / 댓글 */}
+                <div className="return-btn">
+                  <Link to="/Posts">←</Link><h3>돌아가기</h3>
+                </div>
+                <div className="Post-content">
+                  <div className="body">
+                    <h1>{props?.post?.title}</h1>
+                    <p className="body-text">내용 : {props?.post?.content}</p>
+                    {/* TODO : 태그 / 좋아요 / 댓글 */}
+                  </div>
+                  <p className="writer">{props?.post?.author.email}</p>
+                </div>
+                <div className="Post-underbox">
+                  <div className="side-box">
+                    <div className="tags">#고민 #상담</div>
+                    <div className="indicator">좋아요 : 10개</div>
+                  </div>
+                  <hr />
+                  <div className="comment-container"> {/*pagenation*/}
+                    <div className="comment">
+                      <h4>익명용사348</h4>
+                      <p>이게 댓글이구만유</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             );
           }
