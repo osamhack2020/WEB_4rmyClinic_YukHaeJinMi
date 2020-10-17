95f7d0d86344b6c3faa6e8d8c0fc1e3870aff89f
이성빈 <lsb918@uos.ac.kr>
2020-10-17 16:02:48 +0900

    model changed for chat

--- server/clinic/gql/query.py
+++ server/clinic/gql/query.py
@@ -1,14 +1,14 @@
 from graphene import relay, ObjectType, String, Field
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-from .models import User, Post, Comment, Like, Tag
+from .models import User, Post, Comment, Like, Tag, Counsel, Chat
 from api.models import Image
 
 class UserNode(DjangoObjectType):
   class Meta:
     model = User
     interfaces = (relay.Node,)
-    exclude = ("is_admin", "password", "is_active")
+    exclude = ("is_admin", "password", "is_active", "counsel_counselor", "counsel_client", "chat")
     filter_fields = {
       'email': ['icontains'],
       'division': ['icontains'],
@@ -43,12 +43,24 @@ class TagNode(DjangoObjectType):
       'name': ['icontains'],
     }
 
+class CounselNode(DjangoObjectType):
+  class Meta:
+    model=Counsel
+    interfaces = (relay.Node, )
+
+class ChatNode(DjangoObjectType):
+  class Meta:
+    model=Chat
+    interfaces = (relay.Node, )
+
 class Query(ObjectType):
   node = relay.Node.Field()
   user = relay.Node.Field(UserNode)
   post = relay.Node.Field(PostNode)
   tag = relay.Node.Field(TagNode)
-  
+  counsel = relay.Node.Field(CounselNode)
+  chat = relay.Node.Field(ChatNode)
+
   all_tags = DjangoFilterConnectionField(TagNode)
   recent_posts = DjangoFilterConnectionField(PostNode)
   all_users = DjangoFilterConnectionField(UserNode)
